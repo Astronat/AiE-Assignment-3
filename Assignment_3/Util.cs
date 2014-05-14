f0192281839ceec6d60024e656db719f61269b0e
Astronat <c3142113@gmail.com>
2014-05-15 03:41:10 +1000

    Added Util.cs

---
+++ Assignment_3/Util.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Assignment_3 {
+	class Util {
+		public static Color ColorInterpolate (Color colorA, Color colorB, float bAmount) {
+			var aAmount = 1.0f - bAmount;
+			var r = (int)(colorA.R * aAmount + colorB.R * bAmount);
+			var g = (int)(colorA.G * aAmount + colorB.G * bAmount);
+			var b = (int)(colorA.B * aAmount + colorB.B * bAmount);
+
+			return Color.FromNonPremultiplied(r, g, b, 255);
+		}
+	}
+}
