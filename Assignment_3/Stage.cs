ead7e77b8e598af5ab8ceeea44a521c5361c14d9
Astronat <c3142113@gmail.com>
2014-05-15 03:21:01 +1000

    Update(), Draw(), destruction, etc. for Ammo in Stage.cs

--- Assignment_3/Stage.cs
+++ Assignment_3/Stage.cs
@@ -7,11 +7,14 @@ using Microsoft.Xna.Framework.Graphics;
 namespace Assignment_3 {
 	class Stage {
 		private Rectangle bounds;
+
 		public List<Rectangle> GroundChunks = new List<Rectangle>();
+
+		public List<Ammo> AmmoPickups = new List<Ammo>(); 
 
 		private const float LineWidth = 8f;
 
-		public float ScrollSpeed = 1f;
+		public float ScrollSpeed = 2f;
 		public float XPosition = 0f;
 
 		public Stage(Rectangle gameBounds) {
@@ -22,19 +25,24 @@ namespace Assignment_3 {
 		}
 		
 		public void Draw(SpriteBatch sb) {
-			var currentStart = -(int)XPosition;
+			var currentStart = XPosition;
+
+			//Draw each ammo pickup
+			foreach (var a in AmmoPickups) {
+				a.Draw(sb);
+			}
 
 			//Draw each section's background
 			//This is separate from the below so that it doesn't end up drawing over the vertical sections
 			foreach (var t in GroundChunks) {
 				sb.Draw(Game1.OnePxWhite,
-				        new Rectangle(currentStart, bounds.Bottom - t.Height, t.Width,
+				        new Rectangle((int)currentStart, bounds.Bottom - t.Height, t.Width,
 				                      t.Height), Color.FromNonPremultiplied(50,50,50,255));
 				currentStart += t.Width;
 			}
 
 			//Reset the current start position
-			currentStart = -(int)XPosition;
+			currentStart = XPosition;
 
 			//Draw the individual lines that make up the floor
 			for (var i = 0; i < GroundChunks.Count; i++ ) {
@@ -58,26 +66,46 @@ namespace Assignment_3 {
 				DrawLine(sb, new Vector2(currentStart, bounds.Bottom - bottom + (LineWidth / 2)),
 				         new Vector2(currentStart, bounds.Bottom - bottom - leng - (LineWidth / 2)), LineWidth, Color.White);
 			}
+
 		}
 
 		public void Update () {
-			//If the first chunk in the array is completely off screen, remove it and reset the scroll speed
-			if (GroundChunks[0].Width < XPosition) {
-				GroundChunks.RemoveAt(0);
-				XPosition = ScrollSpeed;
-			}
-
 			//The sum of each of the chunks' lengths
 			var totalWidth = GroundChunks.Sum(item => item.Width);
 			
 			//Check if a new chunk is required, generate and add it if it is
-			if (totalWidth - XPosition < bounds.Width + LineWidth) {
-				var rndWidth = Game1.gameRand.Next(70, 160);
-				GroundChunks.Add(new Rectangle(0, bounds.Height - rndWidth, Game1.gameRand.Next(150, bounds.Width / 2), rndWidth));
+			if (totalWidth - Math.Abs(XPosition) < bounds.Width + LineWidth) {
+				var rndHeight = Game1.gameRand.Next(70, 160);
+				var rndWidth = Game1.gameRand.Next(150, bounds.Width/2);
+				
+				GroundChunks.Add(new Rectangle(0, bounds.Height - rndHeight, rndWidth, rndHeight));
+
+				//30% chance to add an ammo pickup to the new chunk; This may need to be tweaked
+				if (Game1.gameRand.NextDouble() > 0.7) {
+					Console.WriteLine("Creating new pickup");
+					AmmoPickups.Add(new Ammo(new Vector2(bounds.Width + LineWidth + (float)(GroundChunks[GroundChunks.Count - 1].Width * Game1.gameRand.NextDouble()), bounds.Height - rndHeight - 60)));
+				}
 			}
 
-			//Update the X position
-			XPosition += ScrollSpeed;
+			//Update each ammo pickup
+			foreach (var a in AmmoPickups) {
+				a.Update(ScrollSpeed);
+			}
+
+			//Remove all dead pickups
+			AmmoPickups.RemoveAll(item => !item.Alive);
+			
+			//The above 2 ammo-related chunks are separate as when they were both in
+			//a for-loop, for some reason it was occasionally doing strange double-dips
+			//into their Update()s on occasion (!?) and basically randomly skipping pickups
+			//forwards a few pixels.
+
+			//If the first chunk in the array is completely off screen, remove it and reset the scroll speed
+			if (GroundChunks[0].Width <= Math.Abs(XPosition)) {
+				XPosition = XPosition + GroundChunks[0].Width - (LineWidth / 2);
+				GroundChunks.RemoveAt(0);
+			} else
+				XPosition -= ScrollSpeed;
 		}
 
 		//Effectively draws a white line between two points
